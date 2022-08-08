1de904f074f712248a45859baf494296b7f1026b
Irissf <iris.seijo@gmail.com>
2022-08-08 19:43:51 +0200

    angular repaso

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reto1';
+  url = 'https://i.pinimg.com/564x/3a/1e/01/3a1e019195cefca5cd839744a347118a.jpg';
+
+  //bidireccional
+  name!: string;
 }
