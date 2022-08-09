80b9fc48a56531d3edef6e623cedc1571d934d79
Irissf <iris.seijo@gmail.com>
2022-08-09 18:30:57 +0200

    todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  {
   title = 'reto1';
   url = 'https://i.pinimg.com/564x/3a/1e/01/3a1e019195cefca5cd839744a347118a.jpg';
 
@@ -14,4 +14,6 @@ export class AppComponent {
 
   //directivas
   cities = ['Vigo','Moaña','Cangas'];
+
+  
 }
